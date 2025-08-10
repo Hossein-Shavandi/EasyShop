4bd704173a00deb6412bf0971731840bad28c78d
Hossein Shavandi <hosseinShavandifront@gmail.com>
2025-08-10 12:31:12 -0700

    Update HeaderGrid.jsx

--- src/components/header/HeaderMobile/HeaderGrid.jsx
+++ src/components/header/HeaderMobile/HeaderGrid.jsx
@@ -3,42 +3,41 @@ import { Link } from "react-router-dom";
 
 const Grids = () => {
   return (
-    <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow-lg z-50 lg:hidden">
-      <ul className="flex justify-around items-center h-14">
-        <Link>
-          <li className="flex flex-col items-center text-gray-600  transition">
+    <nav className="fixed bottom-0 left-0 w-full bg-white border-t border-gray-200 shadow-lg z-50 lg:hidden overflow-x-hidden">
+      <ul className="flex justify-around items-center h-14 w-full px-0">
+        <li className="flex flex-col items-center text-gray-600 transition">
+          <Link to="/" className="flex flex-col items-center px-2">
             <Home size={22} />
-            <span className="text-xs">خانه</span>
-          </li>
-        </Link>
+            <span className="text-xs mt-1">خانه</span>
+          </Link>
+        </li>
 
-        <Link>
-          <li className="flex flex-col items-center text-gray-600  transition">
+        <li className="flex flex-col items-center text-gray-600 transition">
+          <Link to="/categories" className="flex flex-col items-center px-2">
             <Grid size={22} />
-            <span className="text-xs">دسته بندی</span>
-          </li>
-        </Link>
+            <span className="text-xs mt-1">دسته بندی</span>
+          </Link>
+        </li>
 
-        <Link>
-          <li className="flex flex-col items-center text-gray-600  transition relative">
+        <li className="flex flex-col items-center text-gray-600 transition relative">
+          <Link to="/cart" className="flex flex-col items-center relative px-2">
             <ShoppingCart size={22} />
-            <span className="text-xs">سبد خرید</span>
-            <span className="absolute text-center -top-1 -right-2 bg-bg-blue-sky text-white text-[10px] px-1 rounded-full">
+            <span className="text-xs mt-1">سبد خرید</span>
+            <span className="absolute -top-1 -right-3 bg-blue-500 text-white text-[10px] px-1 rounded-full min-w-[16px] text-center">
               3
             </span>
-          </li>
-        </Link>
+          </Link>
+        </li>
 
-        <Link>
-          <li className="flex flex-col items-center text-gray-600  transition">
+        <li className="flex flex-col items-center text-gray-600 transition">
+          <Link to="/account" className="flex flex-col items-center px-2">
             <User size={22} />
-            <span className="text-xs">حساب من</span>
-          </li>
-        </Link>
+            <span className="text-xs mt-1">حساب من</span>
+          </Link>
+        </li>
       </ul>
     </nav>
   );
-}
+};
 
-
-export default Grids
+export default Grids;
